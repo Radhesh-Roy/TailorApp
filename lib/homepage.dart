ec0ad936ff6e9089cd1ccf59df15fb3a1aeac419
Radhesh-Roy <radheshroy0011@gmail.com>
2025-11-10 07:55:41 +0600

    add search

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -13,12 +13,86 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: Color(0xffFFFFFF),
       appBar: AppBar(
-        leading: Image.asset(
-          "assets/profile/Radhesh.jpg",
+        backgroundColor: Color(0xffFFFFFF),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/profile/Radhesh.jpg"),
+          ),
         ),
-        title: const Text("Radhesh", style: TextStyle(fontFamily:"Rj"),),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hello, Radhesh",
+              style: TextStyle(fontFamily: "Rj", fontSize: 16),
+            ),
+            Text(
+              "Good Morning",
+              style: TextStyle(fontFamily: "Rj", fontSize: 12),
+            ),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                color: Color(0xffFFFFFF),
+                boxShadow: [BoxShadow(blurRadius: 3, color: Colors.grey)],
+                shape: BoxShape.circle,
+              ),
+              child: Icon(Icons.notifications),
+            ),
+          ),
+        ],
       ),
-      body: Text("Radhesh", style: TextStyle(fontFamily:"Rj"),),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 3,
+                    color: Color(0xffFFFFFF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(100),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.search),
+                          hintText: "Search",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFFFFF),
+                    boxShadow: [BoxShadow(blurRadius: 2)],
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(Icons.menu),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
